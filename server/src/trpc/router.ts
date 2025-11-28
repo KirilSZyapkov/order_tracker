ddf43bf2a88b526886cf0b7d3e27dd7f2a9b4418
Kiril Zyapkov <appdeveloperjs47@gmail.com>
2025-11-28 10:36:33 +0200

    Add truck router with createNewTruck procedure and update appRouter

--- server/src/trpc/router.ts
+++ server/src/trpc/router.ts
@@ -1,10 +1,12 @@
 import { router } from "./trpc";
 import { shipmentRouter } from "./routes/shipment";
 import { userRouter } from "./routes/user";
+import { truckRouter } from "./routes/truck";
 
 export const appRouter = router({
   shipment: shipmentRouter,
   user: userRouter,
+  truck: truckRouter,
 });
 
 export type AppRouter = typeof appRouter;
--- server/src/trpc/routes/truck.ts
+++ server/src/trpc/routes/truck.ts
@@ -1,6 +1,7 @@
 import { router, publicProcedure } from "../trpc";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import { createNewTruckInput } from "../../types/trpcInputTypes/types";
 
 export const truckRouter = router({
   getAllTrucks: publicProcedure.input(z.object({ organizationName: z.string() })).query(async ({ ctx, input }) => {
@@ -68,6 +69,33 @@ export const truckRouter = router({
         throw new TRPCError({
           code: 'INTERNAL_SERVER_ERROR',
           message: 'Failed to fetch truck',
+        });
+      }
+    }),
+
+  createNewTruck: publicProcedure
+    .input(createNewTruckInput)
+    .mutation(async ({ ctx, input }) => {
+      const userId = ctx.userId;
+      if (!userId) {
+        throw new TRPCError({
+          code: 'UNAUTHORIZED',
+          message: 'User not authenticated',
+        });
+      };
+      try {
+        const newTruck = await ctx.db.truck.create({
+          data: {
+            plateNumber: input.plateNumber,
+            organizationName: input.organizationName,
+          }
+        });
+        return newTruck;
+      } catch (e: unknown) {
+        console.error("Error creating truck:", e);
+        throw new TRPCError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: 'Failed to create truck',
         });
       }
     })
