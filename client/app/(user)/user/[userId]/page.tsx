767e178ee9c8f48adb0d15726c21ef38935b2727
Kiril Zyapkov <appdeveloperjs47@gmail.com>
2026-03-03 19:59:47 +0200

    feat: enhance user page to fetch and display user-specific shipments

--- client/app/(user)/user/[userId]/page.tsx
+++ client/app/(user)/user/[userId]/page.tsx
@@ -3,27 +3,36 @@
 import { apiFetch } from "@/lib/utils";
 import { useAppStore } from "@/store/store";
 import { useState, useEffect } from "react";
+import { ShipmentType } from "@/types/shipmentType";
+
 
 export default function UserPage() {
   const curUser = useAppStore((state) => state.user);
   const isUserLoaded = useAppStore((state) => state.isUserLoaded);
-  const [shipments, setShipments] = useState([]);
+  const [shipments, setShipments] = useState<ShipmentType[]>([]);
 
-  useEffect(()=>{
-    if(!curUser) {
-      const response = apiFetch("/api/shipments/list",{
-        
-      })
+  useEffect(() => {
+    async function fetch() {
+      if (curUser) {
+        const response = await apiFetch<ShipmentType[]>(`/api/shipments/list/${curUser?.id}/${curUser?.organizationName}`);
+        if (response) {
+          setShipments(response);
+        } 
+      }
     }
-  },[curUser])
+    fetch();
+  }, [curUser]);
 
-  if(!isUserLoaded){
+  if (!isUserLoaded) {
     return (
       <div className="flex items-center justify-center">
         <h1 className="font-bold text-2xl py-3">Loading user data...</h1>
-      </div>    
+      </div>
     )
-  }
+  };
+
+  console.log("user, 40", shipments);
+  
 
   return (
     <div>
